0896f666a50e02746f2cbfb50b5f739b8a34002a
diegolima362 <diegolima362@gmail.com>
2020-10-03 15:59:56 -0300

    update colors

--- lib/themes/custom_themes.dart
+++ lib/themes/custom_themes.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
 class CustomThemes {
-  static Color accentColor = Color(0xFF505050);
-  static Color defaultAccentColor = Color(0xFF505050);
+  static bool isDark = false;
+  static Color accentColor = Color(0xFF273c75);
+  static Color defaultAccentColor = Color(0xFF273c75);
+
+  static Color anotherWhite = Color(0xEff5f6fa);
+  static Color white = Color(0xFFf5f6fa);
+  static Color black = Colors.black;
+  static Color anotherBlack = Color(0xFF191919);
 
   static Map<int, Color> swash = {
     50: accentColor.withOpacity(.1),
@@ -24,41 +30,81 @@ class CustomThemes {
         textSelectionColor: accentColor.withAlpha(100),
         textSelectionHandleColor: accentColor,
         primaryColor: accentColor,
-        primaryColorDark: Colors.blueGrey,
+        primaryColorDark: anotherBlack,
         primaryColorBrightness: Brightness.light,
         appBarTheme: AppBarTheme(
           brightness: Brightness.light,
-          color: Colors.white,
+          color: white,
         ),
-        backgroundColor: Colors.white,
-        canvasColor: Colors.white,
-        cardTheme: const CardTheme(color: const Color(0xEEFFFFFF)),
+        dialogBackgroundColor: white,
+        backgroundColor: white,
+        canvasColor: white,
+        cardTheme: CardTheme(color: anotherWhite),
         accentColor: accentColor,
         brightness: Brightness.light,
         primarySwatch: MaterialColor(accentColor.value, swash),
+        toggleableActiveColor: accentColor,
       );
 
   static ThemeData get dark => ThemeData(
-        primaryColor: Colors.grey,
+        primaryColor: anotherWhite,
         primaryColorBrightness: Brightness.dark,
-        primaryColorDark: Colors.blueGrey,
-        cursorColor: Colors.white70,
-        textSelectionColor: Colors.blueGrey[600],
-        textSelectionHandleColor: Colors.white70,
+        primaryColorDark: anotherBlack,
+        cursorColor: anotherWhite,
+        textSelectionColor: anotherWhite.withAlpha(100),
+        textSelectionHandleColor: white,
         appBarTheme: AppBarTheme(
           brightness: Brightness.dark,
-          color: Colors.black,
+          color: black,
         ),
-        dialogBackgroundColor: const Color(0xFF101010),
+        dialogBackgroundColor: anotherBlack,
         toggleableActiveColor: accentColor,
-        backgroundColor: Colors.black,
-        canvasColor: Colors.black,
-        cardTheme: const CardTheme(
-          color: const Color.fromARGB(255, 25, 25, 25),
-          shadowColor: const Color(0xFFAAAAAA),
+        backgroundColor: black,
+        canvasColor: black,
+        cardTheme: CardTheme(
+          color: anotherBlack,
+          shadowColor: defaultAccentColor,
         ),
         accentColor: accentColor,
         brightness: Brightness.dark,
         primarySwatch: MaterialColor(accentColor.value, swash),
       );
+
+  static List<Color> get accentColors => [
+        const Color(0xFF00b894),
+        const Color(0xFF0984e3),
+        const Color(0xFF192a56),
+        const Color(0xFF218c74),
+        const Color(0xFF2f3542),
+        const Color(0xFF44bd32),
+        const Color(0xFF487eb0),
+        const Color(0xFF6D214F),
+        Colors.deepPurple,
+        Colors.pink,
+        const Color(0xFFB33771),
+        const Color(0xFFd63031),
+        const Color(0xFFe84118),
+        const Color(0xFFff4757),
+        const Color(0xFFffa502),
+        Colors.brown
+      ];
+
+  static List<Color> get accentColorsDark => [
+        const Color(0xFF00b894),
+        const Color(0xFF0984e3),
+        const Color(0xFF218c74),
+        const Color(0xFF227093),
+        const Color(0xFF273c75),
+        const Color(0xFF2ed573),
+        const Color(0xFF6D214F),
+        const Color(0xFF74b9ff),
+        const Color(0xFFb2bec3),
+        const Color(0xFFd63031),
+        const Color(0xFFe84118),
+        Colors.cyan,
+        Colors.amber,
+        Colors.pink,
+        Colors.lightGreenAccent,
+        Colors.blueGrey,
+      ];
 }
