a291108f837fb75ec8e2aca46660217dadd5407d
diegolima362 <diegolima362@gmail.com>
2021-01-19 15:08:52 -0300

    update themes module v1.10

--- lib/app/shared/themes/custom_themes.dart
+++ lib/app/shared/themes/custom_themes.dart
@@ -1,14 +1,235 @@
 import 'package:flutter/material.dart';
 
 class CustomThemes {
-  CustomThemes._();
+  List<Color> get darkAccentColores => [
+        const Color(0xffe43f5a),
+        const Color(0xfff0f0f0),
+        const Color(0xff77e2c3),
+        const Color(0xffffaf7b),
+        const Color(0xfff8ca4d),
+        const Color(0xffd9583b),
+        const Color(0xff2a9d8f),
+        const Color(0xff2979ff),
+      ];
 
-  static Color darkAccentColor = Color(0xFFE43F5A);
-  static Color lightAccentColor = Color(0xFF1A1A1A);
+  List<Color> get lightAccentColores => [
+        const Color(0xff141414),
+        const Color(0xffe43f5a),
+        const Color(0xff24314d),
+        const Color(0xffd9583b),
+        const Color(0xff2a9d8f),
+        const Color(0xff2979ff),
+        const Color(0xff006d77),
+        const Color(0xff4d194d),
+      ];
+
+  ThemeData getDark({int colorValue}) {
+    Color accent = darkAccentColores[0];
+
+    if (colorValue != null) {
+      final accentColor = Color(colorValue);
+
+      if (accentColor.alpha != 0) {
+        accent = accentColor;
+      }
+    }
+
+    final bg = Color(0xff141414);
+
+    // final otherWhite = Color(0xFFFEFEFE);
+    final textColor = Color(0xffe8e8e8);
+    final appBarTitle = Color(0xffe8e8e8);
+    // final titleColor = Color(0xffe8e8e8);
+    // final dark = Color(0xff495464);
+    final primary = accent;
+    // final disableIcon = primary.withOpacity(0.5);
+    final selectedColor = accent;
+    final cardColor = Color(0xff1a1a1a);
+    // final barColor = Color(0xff212121);
+    // final bottomBar = Color(0xff1f1f1f);
+    final buttonColor = accent;
+    final iconColor = accent;
+    final dividerColor = Color(0xff303030);
+    final subtitle = Color(0xff495464);
+    final splash = accent;
+    final swash = MaterialColor(accent.value, _getSwash(accent));
+
+    return ThemeData(
+      primarySwatch: swash,
+      brightness: Brightness.dark,
+      accentColorBrightness: Brightness.dark,
+      primaryColorBrightness: Brightness.dark,
+      indicatorColor: textColor,
+      backgroundColor: bg,
+      primaryColor: primary,
+      buttonColor: buttonColor,
+      buttonTheme: ButtonThemeData(
+        buttonColor: buttonColor,
+        textTheme: ButtonTextTheme.normal,
+      ),
+      accentColor: accent,
+      canvasColor: bg,
+      // disabledColor: disableIcon,
+      iconTheme: IconThemeData(color: iconColor),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: bg,
+        // unselectedIconTheme: IconThemeData(color: disableIcon),
+        selectedIconTheme: IconThemeData(color: selectedColor),
+      ),
+      cardColor: cardColor,
+      cardTheme: CardTheme(color: cardColor),
+      dividerColor: dividerColor,
+      splashColor: splash,
+      textTheme: TextTheme(
+        button: TextStyle(
+          color: bg,
+        ),
+        subtitle2: TextStyle(
+          color: subtitle,
+        ),
+        subtitle1: TextStyle(
+          color: textColor,
+        ),
+        bodyText1: TextStyle(
+          fontSize: 16.0,
+          color: textColor,
+        ),
+        bodyText2: TextStyle(
+          fontSize: 16.0,
+          color: textColor,
+        ),
+      ),
+      dialogBackgroundColor: cardColor,
+      dialogTheme: DialogTheme(backgroundColor: cardColor),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: accent,
+        hoverColor: subtitle,
+        focusColor: subtitle,
+        foregroundColor: bg,
+        elevation: 2,
+      ),
+      appBarTheme: AppBarTheme(
+        iconTheme: IconThemeData(color: iconColor),
+        actionsIconTheme: IconThemeData(color: iconColor),
+        color: bg,
+        brightness: Brightness.dark,
+        textTheme: TextTheme(
+          headline6: TextStyle(
+            fontSize: 20.0,
+            color: appBarTitle,
+          ),
+        ),
+        elevation: 1.0,
+      ),
+    );
+  }
+
+  ThemeData getLight({int colorValue}) {
+    Color accent = lightAccentColores[0];
+
+    if (colorValue != null) {
+      final accentColor = Color(colorValue);
+
+      if (accentColor.alpha != 0) {
+        accent = accentColor;
+      }
+    }
+
+    final bg = Color(0xfffafafa);
+
+    // final otherWhite = Color(0xFFFEFEFE);
+    final textColor = Color(0xff141414);
+    // final appBarTitle = Color(0xff1a1a1a);
+
+    // final dark = Color(0xff495464);
+    final primary = accent;
+    // final disableIcon = primary.withOpacity(0.5);
+    final selectedColor = accent;
+    final cardColor = Color(0xffffffff);
+    // final barColor = accent;
+    final buttonColor = accent;
+    final iconColor = accent;
+    final dividerColor = Color(0xffbbbbbb);
+    final subtitle = Color(0xff3a3a3a);
+    final splash = accent;
+    final swash = MaterialColor(accent.value, _getSwash(accent));
+
+    return ThemeData(
+      cardColor: cardColor,
+      primarySwatch: swash,
+      brightness: Brightness.light,
+      accentColorBrightness: Brightness.light,
+      primaryColorBrightness: Brightness.light,
+      indicatorColor: textColor,
+      backgroundColor: bg,
+      primaryColor: primary,
+      buttonColor: buttonColor,
+      buttonTheme: ButtonThemeData(
+        buttonColor: buttonColor,
+        textTheme: ButtonTextTheme.normal,
+      ),
+      accentColor: accent,
+      canvasColor: bg,
+      // disabledColor: disableIcon,
+      iconTheme: IconThemeData(color: iconColor),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: bg,
+        // unselectedIconTheme: IconThemeData(color: disableIcon),
+        selectedIconTheme: IconThemeData(color: selectedColor),
+      ),
+      cardTheme: CardTheme(color: cardColor),
+      dividerColor: dividerColor,
+      splashColor: splash,
+      textTheme: TextTheme(
+        button: TextStyle(
+          color: bg,
+        ),
+        subtitle2: TextStyle(
+          color: subtitle,
+        ),
+        subtitle1: TextStyle(
+          color: textColor,
+        ),
+        bodyText1: TextStyle(
+          fontSize: 16.0,
+          color: textColor,
+        ),
+        bodyText2: TextStyle(
+          fontSize: 16.0,
+          color: textColor,
+        ),
+      ),
+      dialogBackgroundColor: cardColor,
+      dialogTheme: DialogTheme(backgroundColor: cardColor),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: accent,
+        hoverColor: subtitle,
+        focusColor: subtitle,
+        foregroundColor: bg,
+        elevation: 2,
+      ),
+      appBarTheme: AppBarTheme(
+        iconTheme: IconThemeData(color: iconColor),
+        actionsIconTheme: IconThemeData(color: iconColor),
+        color: bg,
+        brightness: Brightness.light,
+        textTheme: TextTheme(
+          headline6: TextStyle(
+            fontSize: 20.0,
+            color: textColor,
+          ),
+        ),
+        elevation: 1.0,
+        shadowColor: textColor,
+      ),
+    );
+  }
 
   // static Color lightAccentColor = Color(0xFF1D3557);
 
-  static Map<int, Color> _getSwash(Color accentColor) {
+  Map<int, Color> _getSwash(Color accentColor) {
     return {
       50: accentColor.withOpacity(.1),
       100: accentColor.withOpacity(.2),
@@ -21,168 +242,5 @@ class CustomThemes {
       800: accentColor.withOpacity(.9),
       900: accentColor.withOpacity(1),
     };
-  }
-
-  static ThemeData get light {
-    final white = Color(0xFFF7F6F1);
-
-    // final otherWhite = Color(0xFFFEFEFE);
-    // final red = Color(0xFFE63946);
-    final textColor = Color(0xFF1A1A1A);
-    final accent = lightAccentColor;
-    final primary = lightAccentColor;
-    final disableIcon = primary.withOpacity(0.5);
-    final selectedColor = lightAccentColor;
-    final cardColor = Color(0xFFFFFFFF);
-    final buttonColor = lightAccentColor;
-    final iconColor = lightAccentColor;
-    final dividerColor = Color(0xFFDDDDDD);
-    final anotherWhite = Color(0xFFAAAAAA);
-    final swash = MaterialColor(accent.value, _getSwash(accent));
-
-    return ThemeData(
-      primarySwatch: swash,
-      brightness: Brightness.light,
-      accentColorBrightness: Brightness.light,
-      primaryColorBrightness: Brightness.light,
-      indicatorColor: textColor,
-      backgroundColor: white,
-      primaryColor: primary,
-      buttonColor: buttonColor,
-      buttonTheme: ButtonThemeData(
-        buttonColor: buttonColor,
-        textTheme: ButtonTextTheme.primary,
-      ),
-      accentColor: accent,
-      canvasColor: white,
-      disabledColor: disableIcon,
-      iconTheme: IconThemeData(color: iconColor),
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        elevation: 0,
-        backgroundColor: white,
-        unselectedIconTheme: IconThemeData(color: disableIcon),
-        selectedIconTheme: IconThemeData(color: selectedColor),
-      ),
-      cardTheme: CardTheme(color: cardColor),
-      dividerColor: dividerColor,
-      textTheme: TextTheme(
-        button: TextStyle(
-          color: white,
-        ),
-        subtitle2: TextStyle(
-          color: anotherWhite,
-        ),
-        subtitle1: TextStyle(
-          color: textColor,
-        ),
-        bodyText1: TextStyle(
-          fontSize: 16.0,
-          color: textColor,
-        ),
-        bodyText2: TextStyle(
-          fontSize: 16.0,
-          color: textColor,
-        ),
-      ),
-      floatingActionButtonTheme: FloatingActionButtonThemeData(
-        backgroundColor: textColor,
-        foregroundColor: white,
-      ),
-      appBarTheme: AppBarTheme(
-        iconTheme: IconThemeData(color: textColor),
-        actionsIconTheme: IconThemeData(color: textColor),
-        color: white,
-        brightness: Brightness.light,
-        textTheme: TextTheme(
-          headline6: TextStyle(
-            fontSize: 24.0,
-            color: textColor,
-          ),
-        ),
-        elevation: 0,
-      ),
-    );
-  }
-
-  static ThemeData get dark {
-    final white = Color(0xFFFEFEFE);
-    final dark = Color(0xFF121212);
-    // final otherWhite = Color(0xFFFEFEFE);
-    // final red = Color(0xFFE63946);
-    final textColor = Color(0xFFEEEEEE);
-    final accent = darkAccentColor;
-    final primary = darkAccentColor;
-    final disableIcon = primary.withOpacity(0.5);
-    final selectedColor = darkAccentColor;
-    final cardColor = Color(0xFF202523);
-    final buttonColor = darkAccentColor;
-    final iconColor = darkAccentColor;
-    final dividerColor = Color(0xFF121212);
-    final anotherWhite = Color(0xFFAAAAAA);
-    final swash = MaterialColor(accent.value, _getSwash(accent));
-
-    return ThemeData(
-      primarySwatch: swash,
-      brightness: Brightness.dark,
-      accentColorBrightness: Brightness.dark,
-      primaryColorBrightness: Brightness.dark,
-      indicatorColor: textColor,
-      toggleableActiveColor: accent,
-      backgroundColor: dark,
-      primaryColor: primary,
-      buttonColor: buttonColor,
-      buttonTheme: ButtonThemeData(
-        buttonColor: buttonColor,
-        textTheme: ButtonTextTheme.primary,
-      ),
-      accentColor: accent,
-      canvasColor: dark,
-      disabledColor: disableIcon,
-      iconTheme: IconThemeData(color: iconColor),
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        elevation: 0,
-        backgroundColor: dark,
-        unselectedIconTheme: IconThemeData(color: disableIcon),
-        selectedIconTheme: IconThemeData(color: selectedColor),
-      ),
-      cardTheme: CardTheme(color: cardColor),
-      dividerColor: dividerColor,
-      textTheme: TextTheme(
-        button: TextStyle(
-          color: white,
-        ),
-        subtitle2: TextStyle(
-          color: anotherWhite,
-        ),
-        subtitle1: TextStyle(
-          color: textColor,
-        ),
-        bodyText1: TextStyle(
-          fontSize: 16.0,
-          color: textColor,
-        ),
-        bodyText2: TextStyle(
-          fontSize: 16.0,
-          color: textColor,
-        ),
-      ),
-      floatingActionButtonTheme: FloatingActionButtonThemeData(
-        backgroundColor: iconColor,
-        foregroundColor: dark,
-      ),
-      appBarTheme: AppBarTheme(
-        iconTheme: IconThemeData(color: textColor),
-        actionsIconTheme: IconThemeData(color: textColor),
-        color: dark,
-        brightness: Brightness.dark,
-        textTheme: TextTheme(
-          headline6: TextStyle(
-            fontSize: 24.0,
-            color: accent,
-          ),
-        ),
-        elevation: 0,
-      ),
-    );
   }
 }
