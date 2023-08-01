c5bcac5910fc1f66a5665bbc8be91def0c8ff2b8
diegolima362 <diegolima362@gmail.com>
2023-08-01 19:41:51 -0300

    add show snackbar extension

--- lib/src/shared/data/extensions/build_context_extensions.dart
+++ lib/src/shared/data/extensions/build_context_extensions.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
 extension AlertsUtil on BuildContext {
-  void showMessage(String message, [Function? onClosed]) {
+  void showMessage(
+    String message, [
+    Function? onClosed,
+    SnackBarAction? action,
+  ]) {
     ScaffoldMessenger.of(this)
-        .showSnackBar(SnackBar(content: Text(message)))
+        .showSnackBar(SnackBar(
+          content: Text(message),
+          action: action,
+        ))
         .closed
         .then((_) async => onClosed?.call());
   }
 
-  void showError(String message, [Function? onClosed]) {
+  void showError(
+    String message, [
+    Function? onClosed,
+  ]) {
     ScaffoldMessenger.of(this)
         .showSnackBar(SnackBar(
           content: Text(
@@ -16,6 +26,30 @@ extension AlertsUtil on BuildContext {
             style: TextStyle(color: colors.onErrorContainer),
           ),
           backgroundColor: colors.errorContainer,
+        ))
+        .closed
+        .then((_) async => onClosed?.call());
+  }
+
+  void showErrorWithAction(
+    String message, {
+    required String label,
+    required void Function() onPressed,
+    Function? onClosed,
+  }) {
+    ScaffoldMessenger.of(this)
+        .showSnackBar(SnackBar(
+          content: Text(
+            message,
+            style: TextStyle(color: colors.onErrorContainer),
+          ),
+          backgroundColor: colors.errorContainer,
+          action: SnackBarAction(
+            label: label,
+            onPressed: onPressed,
+            textColor: colors.errorContainer,
+            backgroundColor: colors.onErrorContainer,
+          ),
         ))
         .closed
         .then((_) async => onClosed?.call());
